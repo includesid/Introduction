fbb51b36197b79c43374cab9d8a6164aa8a7dc30
includesid <depanshuiit2017@gmail.com>
2024-03-29 12:00:11 +0530

    first commit

---
+++ Isprime.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Isprime {
+
+    static boolean checkprime(int n) {
+        boolean flag = true;
+        if (n == 0 || n == 1)
+            return false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = s.nextInt();
+        boolean b = checkprime(n);
+        if (b)
+            System.out.println("Prime");
+        else
+            System.out.println("Not prime");
+    }
+}
