9cff64ae1a49e1607cf12a0456a476ff9e33c67f
includesid <depanshuiit2017@gmail.com>
2024-04-01 14:03:18 +0530

    done

---
+++ GCD_LCM.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GCD_LCM {
+    static int lcm(int a, int b) {
+        int greater = Math.max(a, b);
+        int lower = Math.min(a, b);
+        for (int i = greater;; i += greater) {
+            if (i % lower == 0)
+                return i;
+        }
+    }
+
+    static int hcf(int a, int b) {
+        int res = Math.min(a, b);
+        while (res != 0) {
+            if (a % res == 0 && b % res == 0)
+                return res;
+            res--;
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println("LCM of two numbers are: " + lcm(a, b));
+        System.out.println("HCF of two numbers " + hcf(a, b));
+    }
+}
+// we can do this queation by recursion also.
