8ecd240189c161e51189d4d4880b72e6614b0aef
includesid <depanshuiit2017@gmail.com>
2024-04-02 13:35:40 +0530

    pattern done

---
+++ Patterns/Pattern_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        s.close();
+    }
+}
+
+// Output:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
