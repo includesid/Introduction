ce118750e1b4dbcb62b696b1f4aca1116d5f2e99
includesid <depanshuiit2017@gmail.com>
2024-04-07 11:43:55 +0530

    done

---
+++ Functions/Deci_to_anybase.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Deci_to_anybase {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter two number");
+        int n = s.nextInt();
+        int b = s.nextInt();
+        StringBuilder sb = new StringBuilder("");
+
+        int rem;
+        int q;
+
+        while (n != 0) {
+            rem = n % b;
+            sb = sb.append(rem);
+            n = n / b;
+        }
+
+        sb.reverse();
+        String st = sb.toString();
+        System.out.println(Integer.parseInt(st));
+        s.close();
+    }
+}
