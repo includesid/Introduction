3c0ea3553a9c1b1500980a254cfe8665bdfc0245
includesid <depanshuiit2017@gmail.com>
2024-04-07 12:09:09 +0530

    done

---
+++ Functions/Anybase_to_deci.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Anybase_to_deci {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int numindec = 0;
+        int dig;
+        int i = 0;
+        while (a != 0) {
+            dig = a % 10;
+            numindec += dig * Math.pow(b, i++);
+            a = a / 10;
+        }
+        System.out.println(numindec);
+    }
+}
