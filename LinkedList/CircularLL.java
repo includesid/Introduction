149f6938128b22a2dfa7feac51de439edb199450
Depanshu singh <depanshuiit2017@gmail.com>
2025-02-09 10:57:57 +0530

    added code of CircularLL

--- LinkedList/CircularLL.java
+++ LinkedList/CircularLL.java
@@ -1,9 +1,54 @@
 package LinkedList;
 
+class Node{
+    public int val;
+    public Node next;
 
+    public Node(int value){
+        val = value;
+        next = null;
+    }
+}
+
+class linkedlist{
+    Node head;
+    Node tail;
+
+    public linkedlist(){
+        head = null;
+        tail = null;
+    }
+
+    public void insertAtEnd(int value){
+        
+        Node newnode = new Node(value);
+        if(head == null){
+            head = null;
+            tail = null;
+        }
+        else{
+            tail.next = newnode;
+            tail = newnode;
+            tail.next = head;
+        }
+    }
+    public void print(){
+        Node tmp = head;
+        while(tmp.next != head){
+            System.out.print(tmp.val+ " ");
+            tmp = tmp.next;
+        }
+    }
+
+}
 
 public class CircularLL {
     public static void main(String[] args) {
-        
+        linkedlist ll = new linkedlist();
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(5);
+        ll.insertAtEnd(23);
+        ll.insertAtEnd(12);
+        ll.print();
     }
 }
